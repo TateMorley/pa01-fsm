85fd54f76645cd4892cbe04f5e6b4208b6f8d4d2
giltson1 <josephkabesha@gmail.com>
2025-09-10 18:01:34 -0400

    some changes.

---
+++ state.h
@@ -0,0 +1,16 @@
+//---------------------------------------------------------
+// Assignment : Lab-02 Opening Source
+// Date : 9/10/25
+// 
+// Author : FSM-PA_Team01
+//
+// File Name : closed.c
+//---------------------------------------------------------
+
+#ifndef state_h
+#define state_h
+
+
+
+
+#endif
