2a33af2912c6613ed871e18a2442dfcac4d3e620
TateMorley <morleytp@dukes.jmu.edu>
2025-09-11 08:48:18 -0400

    moved state design from lab to our code

---
+++ state.c
@@ -0,0 +1,20 @@
+//---------------------------------------------------------
+// Assignment : Lab-02 Opening Source
+// Date : 9/10/25
+// 
+// Author : FSM-PA_Team01
+//
+// File Name : closed.c
+//---------------------------------------------------------
+
+#include <stdlib.h>
+#include "state.h"
+
+state_t * default_event_handler()
+{
+  return NULL;
+}
+
+void default_action()
+{
+}
